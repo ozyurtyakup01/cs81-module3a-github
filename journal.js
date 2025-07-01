424e8d27bca0ff034bf17b0c23d0bf2a6d02cdc5
ozyurtyakup01 <ozyurt_yakup01@student.smc.edu>
2025-07-01 14:07:07 -0700

    Added 4 functions from module 3b

--- journal.js
+++ journal.js
@@ -3,3 +3,26 @@ function greet(name)
 {
   return "Hello, " + name + "!";
 }
+// Returns the square of a number
+function square(x) 
+{
+  return x * x;
+}
+
+// Returns true if the number is even
+function isEven(x) 
+{
+  return x % 2 === 0;
+}
+
+// Returns true if the number is odd
+function isOdd(x) 
+{
+  return x % 2 !== 0;
+}
+
+// Multiplies two numbers
+function multiply(a, b) 
+{
+  return a * b;
+}
